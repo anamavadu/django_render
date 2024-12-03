824c1fcde476247ded2a247030927107840a29f8
anamavadu <anamavadu@gmail.com>
2024-12-03 14:50:05 -0500

    Configuración para render

--- personal/settings.py
+++ personal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wm5opn@p$15&fk*o)2zyrxwsqv*23(e55e#j4hq-+rjhi3r_qx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'STATICFILES'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
